83d278d6e77dac44a2e83e042c8ad6646f632bca
luguian <luga@chetong.net>
2025-07-22 10:20:52 +0800

    v2.2.2版本发布 1.优化项目代码架构 2.修复一些问题

--- buildSrc/src/main/kotlin/com/knight/wanandroid/buildsrc/ProjectBuildDencies.kt
+++ buildSrc/src/main/kotlin/com/knight/wanandroid/buildsrc/ProjectBuildDencies.kt
@@ -11,8 +11,8 @@ object ProjectBuildDencies {
     const val APPLICATION_ID = "com.knight.kotlin.wanandroid"
     const val MIN_SDK_VERSION = 22
     const val TARGET_SDK_VERSION = 35
-    const val VERSION_CODE = 12
-    const val VERSION_NAME = "2.2.1"
+    const val VERSION_CODE = 13
+    const val VERSION_NAME = "2.2.2"
     const val APP_MODE = false
 
 
