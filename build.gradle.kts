a83a1ea17de94944c83bf9e8dbed3b049863cec6
luguian <1670483545@qq.com>
2025-10-26 16:24:55 +0800

    删除多余代码

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,11 +27,7 @@ buildscript {
         classpath(com.knight.wanandroid.buildsrc.ProjectPluginDencies.KOTLIN_PLUGIN)
         classpath(com.knight.wanandroid.buildsrc.ProjectPluginDencies.GO_ROUTER_REGISTER)
         classpath(com.knight.wanandroid.buildsrc.ProjectPluginDencies.HILT_PLUGIN)
-       // classpath(com.knight.wanandroid.buildsrc.ProjectPluginDencies.KOTLIN_SERIALIZATION)
         classpath(com.knight.wanandroid.buildsrc.ProjectPluginDencies.ANDROID_AOP)
-        //com.android.tools.r8.internal.Jc: Sealed classes are not supported as program classes
-//        classpath("com.android.tools:r8:8.2.24")
-//        classpath("com.android.tools:r8:8.7.18")
     }
 }
 
